b5606f83e1b4a457c5e1ae13930924d67839865b
Josh Sorensen <jsoresnsen@joshs-mbp.lan>
2024-08-10 16:14:42 -0600

    relocated spotify api client logic

---
+++ lib/spotify/api/client.rb
@@ -0,0 +1,24 @@
+module Spotify
+  module Api
+    class Client
+
+      def self.encoded_credentials
+        Base64.strict_encode64("#{ENV['CLIENT_ID']}:#{ENV['CLIENT_SECRET']}")
+      end
+
+      def self.create_access_token
+        conn = Faraday.new(
+          url: 'https://accounts.spotify.com',
+          headers: {
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'Authorization' => "Basic #{encoded_credentials}"
+          }
+        )
+
+        response = conn.post('/api/token', grant_type: 'client_credentials')
+        JSON.parse(response.body)
+      end
+
+    end
+  end
+end
