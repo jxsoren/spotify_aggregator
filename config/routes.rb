c4905e1dcabf5fac90a053aad016e54c23f98d02
jxsoren <jxsoren@gmail.com>
2024-08-17 15:23:17 -0600

    added login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'spotify/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'login/index'
+  get 'login/login'
+  get 'login/callback'
 end
