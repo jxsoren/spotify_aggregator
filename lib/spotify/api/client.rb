1b593d6ba85036a822755c08185547a0aeabf1a7
jxsoren <jxsoren@gmail.com>
2024-08-17 15:34:34 -0600

    removed non-OAuth2 token retrieval method

--- lib/spotify/api/client.rb
+++ lib/spotify/api/client.rb
@@ -6,32 +6,16 @@ module Spotify
         @access_token = nil
       end
 
-      def create_access_token
-        conn = Faraday.new(
-          url: 'https://accounts.spotify.com',
-          headers: {
-            'Content-Type' => 'application/x-www-form-urlencoded',
-            'Authorization' => "Basic #{encoded_credentials}"
-          }
-        )
-
-        response = conn.post('/api/token', grant_type: 'client_credentials')
-        JSON.parse(response.body)['access_token']
-      end
-
       def get_current_users_playlists
         conn = Faraday.new(
-          url: 'https://api.spotify.com/v1',
+          url: 'https://api.spotify.com',
           headers: {
             'Content-Type' => 'application/json',
-            'Authorization' => "Bearer #{@access_token}"
+            'Authorization' => "Bearer #{session['access_token']}"
           }
         )
 
-        response = conn.get('/me/playlists') do |req|
-          req.params['limit'] = 100
-        end
-
+        response = conn.get('/v1/me/playlists')
         JSON.parse(response.body)
       end
 
